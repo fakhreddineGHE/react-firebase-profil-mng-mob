3c216551217e1fdbf186cc7f552ab31ee00553cb
fakhreddine <fakhreddine.ghedira@esprit.tn>
2018-01-12 00:22:39 +0100

    initial project

---
+++ App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import * as firebase from 'firebase';
+// Initialize Firebase
+const firebaseConfig={
+apiKey: "AIzaSyBrZuw6ELjs-IAhYBEUkFRHjwpI2BB614c",
+    authDomain: "react-mob.firebaseapp.com",
+    databaseURL: "https://react-mob.firebaseio.com",
+    projectId: "react-mob",
+    storageBucket: "react-mob.appspot.com",
+    messagingSenderId: "198438473042"
+};
+
+firebase.initializeApp(firebaseConfig);
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
